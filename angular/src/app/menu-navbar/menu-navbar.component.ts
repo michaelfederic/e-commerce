ac3b6d91061e307b8b51a612c0411d76fb9b45d3
federicm <jonmike350@gmail.com>
2023-03-30 15:23:15 -0400

    Added few dependencies  - @ng-bootstrap/ng-bootstrap": "^14.0.1",  - @popperjs/core": "^2.11.7",
    Added a modal to menu-navbar

--- angular/src/app/menu-navbar/menu-navbar.component.ts
+++ angular/src/app/menu-navbar/menu-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-menu-navbar',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-navbar.component.css']
 })
 export class MenuNavbarComponent {
+  constructor(private modalService: NgbModal){}
 
+  openFullscreen(content: any){
+    this.modalService.open(content, {fullscreen: true})
+  }
 }
